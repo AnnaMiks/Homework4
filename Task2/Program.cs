fe4636337eb4eebaf3691af5d183dfe6c7ffb3e7
AnnaMikshina <wow154@mail.ru>
2023-01-28 20:02:05 +0500

    Задача 2 и 3

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+int Prompt(string message)
+{
+    Console.WriteLine(message + ">");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите число");
+int SumDigits(int value)
+{
+    int result = 0;
+
+    while (value > 0)
+    {
+        result = result +value % 10;
+        value = value / 10;
+    }
+    return result;
+}
+int result = SumDigits(number);
+Console.WriteLine($"Сумма цифр числа равна {result}");
---
+++ Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+//6, 1, 33 -> [6, 1, 33]
+
+int InputInt(string message)
+{
+    Console.WriteLine(message + ">");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int[] GenerateArray(int len)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 1; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 99);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        Console.Write($"{item}\t");
+    }
+    Console.WriteLine();
+}
+int sizeofArray = InputInt("Введите размер массива");
+int[] array = GenerateArray(sizeofArray);
+PrintArray(array);
